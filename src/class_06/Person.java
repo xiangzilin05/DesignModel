02497bcc24d7782d950ff4031fc8ec1a36f1a71a
xiangzilin05 <738448954@qq.com>
2017-07-06 22:15:56 +0800

    装饰模式

--- src/class_06/Person.java
+++ src/class_06/Person.java
@@ -4,4 +4,11 @@ package class_06;
  * Created by xianghui on 2017/7/6.
  */
 public class Person {
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public void WearT
 }
