7055151120aedfd739d4efdfeeb63eff36f25554
xiangzilin05 <738448954@qq.com>
2017-07-09 17:30:50 +0800

    装饰模式

---
+++ src/class_06/Decorate.java
@@ -0,0 +1,13 @@
+package class_06;
+
+/**
+ * Created by xianghui on 2017/7/9.
+ */
+public class Decorate {
+    public static void main(String[] args) {
+        Person xc = new Person("小菜");
+
+        System.out.println("\n 第一种装扮：");
+
+    }
+}
--- src/class_06/Person.java
+++ src/class_06/Person.java
@@ -1,5 +1,7 @@
 package class_06;
 
+import java.net.SocketPermission;
+
 /**
  * Created by xianghui on 2017/7/6.
  */
@@ -10,5 +12,48 @@ public class Person {
         this.name = name;
     }
 
-    public void WearT
+    public void wearTShirts(){
+        System.out.println("大T恤");
+    }
+
+    public void wearBigTrouser(){
+        System.out.println("垮裤");
+    }
+
+    public void wearSneakers(){
+        System.out.println("破球鞋");
+    }
+
+    public void wearSuit(){
+        System.out.println("西装");
+    }
+
+    public void wearTie(){
+        System.out.println("领带");
+    }
+
+    public void wearLeatherShoes(){
+        System.out.println("皮鞋");
+    }
+
+    public void Show(){
+        System.out.println("装扮的" + name);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
